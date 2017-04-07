e97e79e457bc73c3e1d24a365a18624180d9b477
tomamit <4tomamit4@gmail.com>
2017-04-07 10:48:19 +0300

    added comment

--- app/src/main/java/arduino/crane/Manual.java
+++ app/src/main/java/arduino/crane/Manual.java
@@ -10,80 +10,82 @@ import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class Manual extends IntroActivity {
 
-        @Override protected void onCreate(Bundle savedInstanceState){
-            setFullscreen(true);
-            super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setFullscreen(true);
+        super.onCreate(savedInstanceState);
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Getting Started")
-                    .description(R.string.started)
-                    .image(R.drawable.first)
-                    .background(R.color.colorPrimary)
-                    .backgroundDark(R.color.colorPrimaryDark)
-                    .scrollable(true)
-                    .build());
+        setFinishEnabled(false);
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Getting Started")
-                    .description(R.string.started)
-                    .image(R.drawable.first)
-                    .background(R.color.colorPrimary)
-                    .backgroundDark(R.color.colorPrimaryDark)
-                    .scrollable(true)
-                    .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Getting Started")
+                .description(R.string.started)
+                .image(R.drawable.first)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .scrollable(true)
+                .build());
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Connecting")
-                    .description(R.string.connect)
-                    .image(R.drawable.connect)
-                    .background(R.color.white)
-                    .backgroundDark(R.color.whitedark)
-                    .scrollable(true)
-                    .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Getting Started")
+                .description(R.string.started)
+                .image(R.drawable.first)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .scrollable(true)
+                .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Connecting")
+                .description(R.string.connect)
+                .image(R.drawable.connect)
+                .background(R.color.white)
+                .backgroundDark(R.color.whitedark)
+                .scrollable(true)
+                .build());
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Automatic Control")
-                    .description(R.string.auto)
-                    .image(R.drawable.auto)
-                    .background(R.color.colorAccent)
-                    .backgroundDark(R.color.colorAccentDark)
-                    .scrollable(true)
-                    .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Automatic Control")
+                .description(R.string.auto)
+                .image(R.drawable.auto)
+                .background(R.color.colorAccent)
+                .backgroundDark(R.color.colorAccentDark)
+                .scrollable(true)
+                .build());
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Set Up is Needed")
-                    .description(R.string.setup)
-                    .image(R.drawable.setuppic)
-                    .background(R.color.red)
-                    .backgroundDark(R.color.redDark)
-                    .scrollable(true)
-                    .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Set Up is Needed")
+                .description(R.string.setup)
+                .image(R.drawable.setuppic)
+                .background(R.color.red)
+                .backgroundDark(R.color.redDark)
+                .scrollable(true)
+                .build());
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("Manual Control")
-                    .description(R.string.manual)
-                    .image(R.drawable.manual)
-                    .background(R.color.cyan)
-                    .backgroundDark(R.color.cyanDark)
-                    .scrollable(true)
-                    .build());
+        addSlide(new SimpleSlide.Builder()
+                .title("Manual Control")
+                .description(R.string.manual)
+                .image(R.drawable.manual)
+                .background(R.color.cyan)
+                .backgroundDark(R.color.cyanDark)
+                .scrollable(true)
+                .build());
 
-            addSlide(new SimpleSlide.Builder()
-                    .title("That's All!!")
-                    .description(R.string.last)
-                    .image(R.drawable.last)
-                    .background(R.color.blue)
-                    .backgroundDark(R.color.blueDark)
-                    .buttonCtaLabel("Get Started")
-                    .buttonCtaClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            startActivity(new Intent(Manual.this, Devices.class));
-                        }
-                    })
-                    .scrollable(true)
-                    .build());
-        }
+        addSlide(new SimpleSlide.Builder()
+                .title("That's All!!")
+                .description(R.string.last)
+                .image(R.drawable.last)
+                .background(R.color.blue)
+                .backgroundDark(R.color.blueDark)
+                .buttonCtaLabel("Get Started")
+                .buttonCtaClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(Manual.this, Devices.class));
+                    }
+                })
+                .scrollable(true)
+                .build());
+    }
 
 }
 
